f0fd2383e6d311b6adbbc39c7bcab7552215fcc9
Matija Marolin <matijamarolin@hotmail.com>
2020-08-06 00:28:30 +0200

    zakljucek

--- script.js
+++ script.js
@@ -1,16 +1,60 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+//Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//Hide Loading
+function complete() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
 //Get Quote from API
 async function getQuote() {
+    loading();
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
-        console.log(data);
+        if(data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown';
+        } else{
+            authorText.innerText = data.quoteAuthor;
+        }
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+        //Stop Loader, Show Quote
+        complete();
     } catch (error) {
-        getQuote();
-        console.log('opaa, ni pregovora', error);
+        getQuote();        
     }
 }
 
+//Tweet Pregovor
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 //On Load
 getQuote();
